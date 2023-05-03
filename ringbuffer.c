87349f47e8549dbba43b6b716dfc6cf9dcc8dcc7
Dangerous Devices <dangerousdevices@istabepeople.com>
2023-05-03 14:13:05 -0400

    cleanup

--- ringbuffer.c
+++ ringbuffer.c
@@ -1,74 +1,106 @@
 /*
  * A reusable implementation of a ring buffer with timestamps for FIFO usage
  */
-#include "config.h"
-#include "ringbuffer.h"
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <errno.h>
+#include "config.h"
+#include "ringbuffer.h"
 
-RingBuffer* rb_create(int max_size) {
-    RingBuffer* buffer = malloc(sizeof(RingBuffer));
-    buffer->buffer = malloc(max_size * sizeof(void*));
-    buffer->head = 0;
-    buffer->tail = 0;
+rb_buffer_t *rb_create(int max_size) {
+    rb_buffer_t *buffer = malloc(sizeof(rb_buffer_t));
+    buffer->head = NULL;
+    buffer->tail = NULL;
     buffer->max_size = max_size;
     buffer->current_size = 0;
     return buffer;
 }
 
-void rb_destroy(RingBuffer* buffer) {
-    free(buffer->buffer);
+void rb_destroy(rb_buffer_t *buffer) {
+    rb_node_t *current = buffer->head;
+    while (current != NULL) {
+        rb_node_t *next = current->next;
+        free(current);
+        current = next;
+    }
     free(buffer);
 }
 
-int rb_is_buffer_full(RingBuffer* buffer) {
-    return buffer->current_size == buffer->max_size;
-}
+void rb_add(rb_buffer_t *buffer, void *data) {
+    struct timespec timestamp;
+    clock_gettime(CLOCK_MONOTONIC, &timestamp);
+    rb_node_t *node = malloc(sizeof(rb_node_t));
+    node->data = data;
+    node->timestamp = timestamp;
+    node->next = NULL;
 
-int rb_is_buffer_empty(RingBuffer* buffer) {
-    return buffer->current_size == 0;
-}
-
-void rb_push(RingBuffer* buffer, void* data) {
-    if (rb_is_buffer_full(buffer)) {
-        printf("Ring buffer is full, dropping oldest item.\n");
-        free(buffer->buffer[buffer->tail]);
-        buffer->tail = (buffer->tail + 1) % buffer->max_size;
+    if (buffer->current_size == 0) {
+        buffer->head = node;
+        buffer->tail = node;
+        buffer->current_size++;
+        return;
+    }
+    if (buffer->current_size == buffer->max_size) {
+        rb_node_t *next_head = buffer->head->next;
+        free(buffer->head);
+        buffer->head = next_head;
         buffer->current_size--;
     }
-    buffer->buffer[buffer->head] = data;
-    buffer->head = (buffer->head + 1) % buffer->max_size;
+
+    buffer->tail->next = node;
+    buffer->tail = node;
     buffer->current_size++;
 }
 
-void* rb_pop(RingBuffer* buffer) {
-    if (rb_is_buffer_empty(buffer)) {
+void *rb_get_most_recent(rb_buffer_t *buffer) {
+
+    if (buffer->current_size == 0) {
         printf("Ring buffer is empty.\n");
         return NULL;
     }
-    void* data = buffer->buffer[buffer->tail];
-    buffer->tail = (buffer->tail + 1) % buffer->max_size;
-    buffer->current_size--;
-    return data;
+
+    rb_node_t *current = buffer->head;
+    rb_node_t *latest_node = current;
+
+    while (current != NULL) {
+        if (current->timestamp.tv_sec > latest_node->timestamp.tv_sec ||
+            (current->timestamp.tv_sec == latest_node->timestamp.tv_sec &&
+             current->timestamp.tv_nsec > latest_node->timestamp.tv_nsec)) {
+            latest_node = current;
+        }
+        current = current->next;
+    }
+    return latest_node->data;
 }
 
-#if	defined(__TEST__)
-int main(int argc, char **argv) {
-    RingBuffer* buffer = rb_create(5);
-    for (int i = 0; i < 10; i++) {
-        time_t timestamp = time(NULL);
-        char* data = malloc(sizeof(char) * 20);
-        sprintf(data, "Item %d at %ld", i, timestamp);
-        rb_push(buffer, data);
+void *rb_get_range(rb_buffer_t *buffer, int start, int count) {
+    if (buffer->current_size == 0) {
+        printf("Ring buffer is empty.\n");
+        return NULL;
     }
-    while (!rb_is_buffer_empty(buffer)) {
-        char* data = (char*) rb_pop(buffer);
-        printf("%s\n", data);
-        free(data);
+    if (start < 0 || start >= buffer->current_size) {
+        printf("Invalid start index.\n");
+        return NULL;
     }
-    rb_destroy(buffer);
-    return 0;
+    if (count < 1 || start + count > buffer->current_size) {
+        printf("Invalid count.\n");
+        return NULL;
+    }
+    void **array = malloc(count  *sizeof(void*));
+    rb_node_t *current = buffer->head;
+
+    int i = 0;
+
+    while (i < start) {
+        current = current->next;
+        i++;
+    }
+
+    for (i = 0; i < count; i++) {
+        array[i] = current->data;
+        current = current->next;
+    }
+
+    return array;
 }
-#endif	// __TEST__
--- ringbuffer.h
+++ ringbuffer.h
@@ -1,26 +1,30 @@
 #if	!defined(_ringbuffer_h)
 #define	_ringbuffer_h
 #include <time.h>
-#ifdef __cplusplus
 
+#ifdef __cplusplus
 extern "C" {
 #endif
-   typedef struct {
-      void** buffer;
-      int head;
-      int tail;
+   typedef struct rb_node {
+      void *data;
+      struct timespec timestamp;
+      struct rb_node *next;
+   } rb_node_t;
+
+   typedef struct rb_buffer {
+      rb_node_t *head;
+      rb_node_t *tail;
       int max_size;
       int current_size;
-      struct timespec timestamp;
-   } RingBuffer;
+   } rb_buffer_t;
 
-   extern RingBuffer* rb_create(int max_size);
-   extern void rb_destroy(RingBuffer* buffer);
-   extern int rb_is_buffer_full(RingBuffer* buffer);
-   extern int rb_is_buffer_empty(RingBuffer* buffer);
-   extern void rb_push(RingBuffer* buffer, void* data);
-   extern int rb_is_buffer_full(RingBuffer* buffer);
+   extern rb_buffer_t* rb_create(int max_size);
+   extern void rb_destroy(rb_buffer_t* buffer);
+   extern void rb_add(rb_buffer_t* buffer, void* data);
+   extern void* rb_get_most_recent(rb_buffer_t* buffer);
+   extern void* rb_get_range(rb_buffer_t* buffer, int start, int count);
 #ifdef __cplusplus
-};
+}
 #endif
+
 #endif	// !defined(_ringbuffer_h)
